a526a6fbd0f79375080cc12b888db15364d790f9
Zeke Kaufman <zeke_kaufman@yahoo.com>
2011-02-15 15:33:46 +0000

    - Created all necessary classes to read in entire header of .im file. Not yet being used.
    git-svn-id: https://zoomwhee.mgh.harvard.edu/svn_devel/OpenMIMS/trunk@533 ea379df5-90a0-4b1b-ac45-2d7255ffb434

--- src/com/nrims/data/Opener.java
+++ src/com/nrims/data/Opener.java
@@ -5,7 +5,7 @@ import java.io.*;
 /**
  * Class responsible for opening MIMS files.
  * 
-**/
+ **/
 public interface Opener {
 
     // Mims specific fields.
@@ -33,7 +33,7 @@ public interface Opener {
 
     public int getWidth();
 
-    public int getHeight();   
+    public int getHeight();
 
     public Object getPixels(int index) throws IndexOutOfBoundsException, IOException;
 
@@ -252,7 +252,7 @@ public interface Opener {
             }
             if (sple_pos_y != 0) {
                 info += "Analysis.PositionY=" + sple_pos_y + "\n";
-            // if(data_included != 0) info += "Analysis.data_included="+data_included+"\n";
+                // if(data_included != 0) info += "Analysis.data_included="+data_included+"\n";
             }
             if (analysis_name.length() > 0) {
                 info += "Analysis.Name=" + analysis_name + "\n";
@@ -398,6 +398,473 @@ public interface Opener {
             }
             return info;
         }
+    }
+
+    class CalCond {
+
+        int n_delta;
+        int np_delta;
+        int tps_comptage;
+        int nb_cycles;
+        double no_used2;
+        double cal_ref_mass;
+        String symbol;
+    }
+
+    class PolyList {
+
+        String structname;
+        int nb_poly;
+        PolyAtomic polyatomic;
+    }
+
+    class MaskNano {
+
+        int nVersion;
+        int nRegulationMode;
+        int nMode;
+        int nGrainMode;
+        int nSemiGraphicMode;
+        int nDeltaX;
+        int nDeltaY;
+        int nNX_max;
+        int nNY_max;
+        int nNX_low;
+        int nNX_high;
+        int nNY_low;
+        int nNY_high;
+        int nNX_lowB;
+        int nNX_highB;
+        int nNY_lowB;
+        int nNY_highB;
+        int nType_detector;
+        int nElectron_scan;
+        int nScanning_mode;
+        int nBlanking_comptage;
+        int nCheckAvailable;
+        int nCheckStart;
+        int nCheckFrequency;
+        int nNbBField;
+        int nPrintRed;
+        TabBFieldNano BFieldTab;
+        SecIonBeamNano horizontalSibcParam, verticalSibcParam;
+        int nSibcBfieldInd;
+        int nSibcSelected;
+        EnergyNano AecParam;
+        int nAecBFieldInd;
+        int nAecSelected;
+        int nAecFrequency;
+        int nE0SCenterBFieldInd;
+        E0SNano E0SCenterParam;
+        int nE0SCenterSelected;
+        int nAecWt;
+        int nPreSputRaster;
+        int nE0pOffsetForCent;
+        int nE0sCenterNbPoints;
+        int nBaselineNbMeas;
+        double dBaselinePdOffset;
+        int nBaselineFrequency;
+        String zDummy;
+    }
+
+    class TabBFieldNano {
+
+        int nFlagBFieldSelected;
+        int nBField;
+        int nWT;
+        int nCTperPixel;
+        double dCTperPoint;
+        int nComputed;
+        int nE0xOffset;
+        int nQVal;
+        int nLF4Val;
+        int nHexVal;
+        int nNbFrame;
+        double noused;
+        TabTrolleyNano mTrolleyTab;
+        PHDTrolleyNano PhdTrolleyTab;
+    }
+
+    class PHDTrolleyNano {
+
+        int nIsUsedForPhdScan;
+        int nStartDacThr;
+        int nDacStep;
+        int nPointNumber;
+        int nCountTime;
+        int nNbscan;
+    }
+
+    class TabTrolleyNano {
+
+        String pszSymbol;
+        double dAMU;
+        double dRadius;
+        int nNegPlate;
+        int nPosPlate;
+        int nDetector;
+        int nOutSlit;
+        int nFlagTrolleyValidad;
+        int nNum;
+        int nPicNum;
+        int nRefPicNum;
+        double dPolarizationVal;
+        double dStartVoltage;
+        int nStartDacPlate1;
+        int nStartDacPlate2;
+        int nDacStep;
+        int nPointNumber;
+        int nCountTime;
+        int nIsUsedForBaseline;
+        double d50PercentWidth;
+        int nEdgesMethod;
+        int nApcCountTime;
+        int nIsUsedForSecIonBeamCentering;
+        int nUnitCorrection;
+        double deflectionVal;
+        int nIsUsedForEnergycentering;
+        int nIsUsedForE0Centering;
+    }
+
+    class E0SNano {
+
+        int nDetId;
+        int nStartDac;
+        int nDacStep;
+        int nE0SCentCountTime;
+        double dStartValue;
+        double d80PercentWidth;
+    }
+
+    class AnalyticalParamNano {
+
+        String pszNomStruct;
+        int nRelease;
+        int nIsN50Large;
+        int notused1;
+        int notused2;
+        String pszComment;
+        ApPrimaryNano prim;
+        ApSecondaryNano seco;
+    }
+
+    class ApPrimaryNano {
+
+        String pszIon ;             /* Source */
+        int nPrimCurrentT0 ;        /* Primary current at t = 0 in pA */
+        int nPrimCurrentTEnd ;      /* Primary current at t = End in pA */
+        int nPrimLDuo ;             /* LDuo in V */
+        int nPrimL1 ;               /* L1 in V */
+        int nDduoPos ;              /* Dduo Position : 0(not used)/1/2/3/4 */
+        int nDduoTabValue ;         /* Positions values*/
+        int nD0Pos ;                /* D0 Position : 0(not used)/1/2/3/4 */
+        int nD0TabValue ;           /* Positions values */
+        int nD1Pos ;                /* D1 Position : 0(not used)/1/2/3/4 */
+        int nD1TabValue ;           /* Positions values */
+        double dRaster ;            /* Raster size in um */
+        double dOct45 ;             /* Octopole 45 in V */
+        double dOct90 ;             /* Octopole 90 in V */
+        double dPrimaryFoc ;        /* E0P in V */
+        String pszAnalChamberPres ; /* Analysis chamber pressure in torr*/
+        /*------------------------------- RELEASE 3 -------------------------------*/
+        int nPrimL0 ;               /* L0 in V */
+        /*------------------------------- RELEASE 4 -------------------------------*/
+        int nCsHv ;                 /* Cs Hv in V */
+        int nDuoHv ;                /* Duo Hv in V */
+        int nUnusedTab ;            /* Unused*/
+
+    }
+
+    class ApSecondaryNano {
+
+        double dHVSample;           /* E0W in V */
+        int nESPos;                 /* Entrance Slit Position :  0(not used)/1/2/3/4/5 */
+        int nESTabWidthValue;       /* Entrance Slit Positions Width */
+        int nESTabHeightValue;      /* Entrance Slit Positions Height */
+        int nASPos;                 /* Aperture Slit Position :  0(not used)/1/2/3/4/5 */
+        int nASTabWidthValue;       /* Aperture Slit positions Width */
+        int nASTabHeightValue;      /* Aperture Slit positions Height */
+        double dEnrjSPosValue;      /* Energy Slit Position */
+        double dEnrjSWidthValue;    /* Energy Slit Width */
+        int nExSFCPos;              /* Exit Slit FC Position :1/2/3 */
+        int nExSFCType;             /* Exit Slit FC type : 0 Normal/1 Large */
+        int nExSFCTabWidthValue;    /* Exit Slit FC positions Width : [ Type ][ Pos ] */
+        int nExSFCTabHeightValue;   /* Exit Slit FC positions Height : [ Type ][ Pos ] */
+        int nExSEM1Pos;             /* Exit Slit EM1 Position :1/2/3 */
+        int nExSEM1Type;            /* Exit Sd ../aplit EM1 Type : 0 Normal/1 Large*/
+        int nExSEM1TabWidthValue;   /* Exit Slit EM1 positions Width : [ Type ][ Pos ] */
+        int nExSEM1TabHeightValue;  /* Exit Slit EM1 positions Height : [ Type ][ Pos ] */
+        int nExSEM2Pos;             /* Exit Slit EM2 Position :1/2/3 */
+        int nExSEM2Type;            /* Exit Slit EM2 Type : 0 Normal/1 Large*/
+        int nExSEM2TabWidthValue;   /* Exit Slit EM2 positions Width : [ Type ][ Pos ] */
+        int nExSEM2TabHeightValue;  /* Exit Slit EM2 positions Height : [ Type ][ Pos ] */
+        int nExSEM3Pos;             /* Exit Slit EM3 Position :1/2/3 */
+        int nExSEM3Type;            /* Exit Slit EM3 Type : 0 Normal/1 Large*/
+        int nExSEM3TabWidthValue;   /* Exit Slit EM3 positions Width : [ Type ][ Pos ] */
+        int nExSEM3TabHeightValue;  /* Exit Slit EM3 positions Height : [ Type ][ Pos ] */
+        int nExSEM4Pos;             /* Exit Slit EM4 Position :1/2/3 */
+        int nExSEM4Type;            /* Exit Slit EM4 Type : 0 Normal/1 Large*/
+        int nExSEM4TabWidthValue;   /* Exit Slit EM4 positions Width : [ Type ][ Pos ] */
+        int nExSEM4TabHeightValue;  /* Exit Slit EM4 positions Height : [ Type ][ Pos ] */
+        int nExSEM5Pos;             /* Exit Slit EM5 Position :1/2/3 */
+        int nExSEM5Type;            /* Exit Slit EM5 Type : 0 Normal/1 Large*/
+        int nExSEM5TabWidthValue;   /* Exit Slit EM5 positions Width : [ Type ][ Pos ] */
+        int nExSEM5TabHeightValue;  /* Exit Slit EM5 positions Height : [ Type ][ Pos ] */
+        double dExSLDWidhtPos;	   /* Exit Slit LD slit vernier position */
+        double dExSLDWidhtValueA; 	/* Exit Slit LD coefficient A */
+        double dExSLDWidhtValueB; 	/* Exit Slit LD coefficient B */
+        double dSecondaryFoc;       /* E0S in V */
+        String pszMultiColChamPres; /* Multicollection chamber */
+        int	nFCsPosBackground ;     /* FCS Positive Background */
+        int	nFCsNegBackground ;     /* FCS Negative Background */
+        double	dEM1Yield ;          /* EM1 Yield */
+        int	nEM1Background ;        /* EM1 Background */
+        int	nEM1DeadTime ;          /* EM1 Dead Time */
+        double	dEM2Yield ;          /* EM2 Yield */
+        int	nEM2Background ;        /* EM2 Background */
+        int	nEM2DeadTime ;          /* EM2 Dead Time */
+        double	dEM3Yield ;          /* EM3 Yield */
+        int	nEM3Background ;        /* EM3 Background */
+        int	nEM3DeadTime ;          /* EM3 Dead Time */
+        double	dEM4Yield ;          /* EM4 Yield */
+        int	nEM4Background ;        /* EM4 Background */
+        int	nEM4DeadTime ;          /* EM4 Dead Time */
+        double	dEM5Yield ;          /* EM5 Yield */
+        int	nEM5Background ;        /* EM5 Background */
+        int	nEM5DeadTime ;          /* EM5 Dead Time */
+        double	dLDYield ;           /* LD Yield */
+        int	nLDBackground ;         /* LD Background */
+        int	nLDDeadTime ;           /* LD Dead Time */
+        int	nExSEM4BPos ;           /* Exit Slit EM4B Position :1/2/3 */
+        int	nExSEM4BType ;          /* Exit Slit EM4B Type : 0 Normal / 1 Large */
+        int	nExSEM4BTabWidthValue ; /* Exit Slit EM4B positions Width : [ Type ] [ Pos ] */
+        int	nExSEM4BTabHeightValue ;/* Exit Slit EM4B positions Height : [ Type ] [ Pos ] */
+        double	dEM4BYield ;         /* EM4B Yield */
+        int	nEM4BBackground ;       /* EM4B Background */
+        int	nEM4BDeadTime ;         /* EM4B Dead Time */
+        int	nUnusedTab ;            /* Unused */
+
+    }
+
+    /**
+     * Contains the Secondary Ion Beam Centering
+     * parameter information.(size = 40 bytes)
+     */
+    class SecIonBeamNano {
+
+        int m_nDetId;               /* Detector Id (-1 If none)*/
+        int m_nStartDac;            /* Start DAC */
+        int m_nDacStep;             /* Dac step */
+        int m_nUnused1;             /* For Byte Alignement */
+
+        /* For Automatic Beam Centering */
+        double m_dStartValue;       /* Start value */
+        double m_d50PerCentWidth;	/* 50% Width in Volts */
+        int m_nAbcCountTime;        /* Counting Time per point multiple of 10ms */
+        int m_nUnused2;             /* For Byte Alignement */
+
+    }
+
+    /**
+     * Contains the Energy Centering parameter information.(size = 40 bytes)
+     */
+    class EnergyNano {
+
+        /* Energy Acquisition */
+        int m_nDetId;               /* Detector Id (-1 If none)*/
+        int m_nStartDac;            /* Start DAC */
+        int m_nDacStep;             /* Dac step */
+        int m_nUnused1;             /* For Byte Alignement */
+
+        /* For Automatic Centering */
+        double m_dStartValue;       /* Start value */
+        double m_dDelta;            /* Delta between max and 10% in Volts */
+        int m_nAecCountTime;        /* Counting Time per point multiple of 10ms */
+        int m_nUnused2;             /* For Byte Alignement */
+
+    }
+
+    /**
+     * Contains analytical parameters information.
+     */
+    class AnalysisParamNano {
+
+        String pszNomStruct;        /* Structure name */
+        int nRelease;               /* Data release version */
+        int nIsN50Large;            /* N50 Large Flag 0=Standard/1=Large */
+        int nUnused2;               /* Unused */
+        int nUnused3;               /* Unused */
+        String pszComment;          /* Comment */
+        ApPrimaryNano prim;       /* Primary Analytical Parameters */
+        ApSecondaryNano seco ;    /* Secondary Analytical Parameters */
+
+    }
+
+    class DefAnalysisBis {
+
+        int	magic;                  /* Magic number = MAGIC_DEFB=2306 */
+        int	release;                /* Version = RELEASE_DEFB=1 */
+        String	filename ;           /* file name */
+        String	matrice ;            /* matrix name */
+        int	sigref_mode;            /* 0: manual 1: auto */
+        int	sigref_nbptsdm;         /* sigref auto nbpts deltam */
+        int	sigref_nbdm;            /* sigref auto nb deltam */
+        int	sigref_ct_scan;         /* scanning count tim x 0,1sec */
+        int	sigref_ct_meas;         /* measuring count tim sec */
+        int	sigref_tps_pulve;       /* time beam=ON during sigref */
+        int	eps_recentrage;         /* EPS Centering 0=NO/1=YES */
+        int	eps_flag;               /* EPS 0=NO/ 1=YES */
+        DefEps	eps;              /* IMS peak switching */
+        int	sple_rot_flag;          /* sample rotation 0=NO/ 1=YES */
+        int	sple_rot_speed;         /* rot speed tr/mn */
+        int	sple_rot_acq_sync;      /* 0:no 1:yes */
+        String	sample_name ;        /* sample name */
+        String	experience ;         /* experience name */
+        String	method ;             /* method name */
+        String	no_used ;
+
+    }
+
+    /**
+     * Contains new analytical parameters information.
+     */
+    class AnalysisParam {
+
+        /*------------------------------- RELEASE 5 -------------------------------*/
+        String pszNomStruct;        /* Structure name Anal_param_nano_bis *//* Det 6 */
+        int nExSEM6Pos;             /* Exit Slit EM6 Position :1/2/3 */
+        int nExSEM6Type;            /* Exit Slit EM6 Type : 0 Normal / 1 Large */
+        int nExSEM6TabWidthValue;   /* Exit Slit EM6 positions Width : [ Type ] [ Pos ] */
+        int nExSEM6TabHeightValue;  /* Height : [ Type ] [ Pos ] */
+        double dEM6Yield;           /* EM6 Yield */
+        int nEM6Background;         /* EM6 Background */
+        int nEM6DeadTime;           /* EM6 Dead Time */
+
+        /* Det 7 */
+        int nExSEM7Pos;             /* Exit Slit EM7 Position :1/2/3 */
+        int nExSEM7Type;            /* Exit Slit EM7 Type : 0 Normal / 1 Large */
+        int nExSEM7TabWidthValue;   /* Exit Slit EM7 positions Width : [ Type ] [ Pos ] */
+        int nExSEM7TabHeightValue;  /* Height : [ Type ] [ Pos ] */
+        double dEM7Yield;           /* EM7 Yield */
+        int nEM7Background;         /* EM7 Background */
+        int nEM7DeadTime;           /* EM7 Dead Time */
+
+        /* Exit Slit XLarge */
+        /* XLarge Exit Slit EM1 positions Width & Height : [ Pos ] */
+        int nXlExSEM1TabWidthValue;
+        int nXlExSEM1TabHeightValue;
+        /* XLarge Exit Slit EM2 positions Width & Height : [ Pos ] */
+        int nXlExSEM2TabWidthValue;
+        int nXlExSEM2TabHeightValue;
+        /* XLarge Exit Slit EM3 positions Width & Height : [ Pos ] */
+        int nXlExSEM3TabWidthValue;
+        int nXlExSEM3TabHeightValue;
+        /* XLarge Exit Slit EM4 positions Width & Height : [ Pos ] */
+        int nXlExSEM4TabWidthValue;
+        int nXlExSEM4TabHeightValue;
+        /* XLarge Exit Slit EM5 positions Width & Height : [ Pos ] */
+        int nXlExSEM5TabWidthValue;
+        int nXlExSEM5TabHeightValue;
+        /* XLarge Exit Slit EM6 positions Width & Height : [ Pos ] */
+        int nXlExSEM6TabWidthValue;
+        int nXlExSEM6TabHeightValue;
+        /* XLarge Exit Slit EM7 positions Width & Height : [ Pos ] */
+        int nXlExSEM7TabWidthValue;
+        int nXlExSEM7TabHeightValue;
+
+        /* Pre sput Preset */
+        ApPresetSlit PreSputPresetSlit;
+        ApPresetLens PreSputPresetLens;
+
+        /* Acq Preset */
+        ApPresetSlit AcqPresetSlit;
+        ApPresetLens AcqPresetLens;
+
+        /*------------------------------- RELEASE 6 -------------------------------*/
+        double dEMTICYield;         /* EMTIC Yield */
+        int nEMTICBackground;       /* EMTIC Background */
+        int nEMTICDeadTime;         /* EMTIC Dead Time */
+        int nFC1PosBackground;      /* FC1 Positive Background */
+        int nFC1NegBackground;      /* FC1 Negative Background */
+        int nFC2PosBackground;      /* FC2 Positive Background */
+        int nFC2NegBackground;      /* FC2 Negative Background */
+        int nFC3PosBackground;      /* FC3 Positive Background */
+        int nFC3NegBackground;      /* FC3 Negative Background */
+        int nFC4PosBackground;      /* FC4 Positive Background */
+        int nFC4NegBackground;      /* FC4 Negative Background */
+        int nFC5PosBackground;      /* FC5 Positive Background */
+        int nFC5NegBackground;      /* FC5 Negative Background */
+        int nFC6PosBackground;      /* FC6 Positive Background */
+        int nFC6NegBackground;      /* FC6 Negative Background */
+        int nFC7PosBackground;      /* FC7 Positive Background */
+        int nFC7NegBackground;      /* FC7 Negative Background */
+        int nDet1Type;              /* Det type (0=EM/1=FC) */
+        int nDet2Type;              /* Det type (0=EM/1=FC) */
+        int nDet3Type;              /* Det type (0=EM/1=FC) */
+        int nDet4Type;              /* Det type (0=EM/1=FC) */
+        int nDet5Type;              /* Det type (0=EM/1=FC) */
+        int nDet6Type;              /* Det type (0=EM/1=FC) */
+        int nDet7Type;              /* Det type (0=EM/1=FC) */
+        int nUnusedTab;
+
+    }
+
+    /**
+     * Contains IMS EPS definition information.
+     */
+    class DefEps {
+
+        int	central_energy;
+        int	field;                  /* central mass b field */
+        PolyAtomic central_mass;    /* central mass */
+        PolyAtomic reference_mass;  /* reference mass */
+        double	tens_tube;           /* of the reference mass */
+        double	max_var_tens_tube;
+
+    }
+
+    /**
+     * Contains Preset lens definition.
+     */
+    class ApPresetLens {
+
+        ApPresetDef PresetInfo ;    /* Preset definition */
+        ApParamPreset ParamTab ;    /* Preset param */
+
+    }
+
+    /**
+     * Contains Preset Slit definition.
+     */
+    class ApPresetSlit {
+
+        ApPresetDef PresetInfo ;    /* Preset definition */
+        ApParamPreset ParamTab ;    /* Preset param */
+
+    }
+
+    /**
+     * Contains parameters for Preset information.
+     */
+    class ApParamPreset {
+        
+        int	nId ;                   /* Parameter Id (par_dfp.h) */
+        int	nValue ;                /* Parameter DAC value */
+        String	szName ;             /* Parameter name */
+
+    }
+
+    /**
+     * Contains Preset definition.
+     */
+    class ApPresetDef {
+
+        String	szFileName ;         /* ISF File name */
+        String	szName ;             /* Preset name */
+        String szDateCalib ;        /* Preset name */
+        int	nIsSelected ;           /* Selection flag */
+        int	nNbParam ;              /* Nb param preset */
+
     }
 
 }
