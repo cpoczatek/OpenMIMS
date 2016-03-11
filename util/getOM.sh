a6e4d4abd2e3f8c9bcca6599db850a88b2416631
Collin Poczatek <collin.pocatek@gmail.com>
2016-03-11 13:07:13 -0500

    added .desktop file creation

--- util/getOM.sh
+++ util/getOM.sh
@@ -32,9 +32,7 @@ cd ~/bin/
 ln -s ./Fiji.app/ImageJ-linux64 ./fiji
 ln -s ./Fiji.app/lib/runOpenMIMS.sh ./openmims
 
-# sketch of handling the .desktop file
-# cp ./Fiji.app/lib/OpenMIMS.desktop ~/.local/share/applications/
-
+# check if ~/bin in in path
 if [[ ":$PATH:" == *":$HOME/bin:"* ]]; then
     echo "~/bin/ is in path"
 else
@@ -42,3 +40,30 @@ else
     echo -e "\n\n# Added by OpenMIMS install script" >> ~/.profile
     echo -e "PATH=$PATH:~/opt/bin" >> ~/.profile
 fi
+
+# sketch of handling the .desktop file
+echo "${green} Create .desktop file and add .im and .nrrd mimetypes...${reset}"
+
+cat > ~/.local/share/applications/OpenMIMS.desktop <<- EOM
+#edited from Fiji.desktop
+Version=1.0
+Name=OpenMIMS
+GenericName=OpenMIMS
+X-GNOME-FullName=OpenMIMS
+Comment=MIMS Image Analysis
+Type=Application
+Categories=Education;Science;ImageProcessing;
+Terminal=false
+StartupNotify=true
+MimeType=application/x-extension-im;application/x-extension-nrrd;
+EOM
+
+echo "Icon=$(readlink -e ~/bin/Fiji.app/images/icon.png)" >> ~/.local/share/applications/OpenMIMS.desktop
+echo "Exec=$(readlink -e ~/bin/Fiji.app/lib/runOpenMIMS.sh) %F" >> ~/.local/share/applications/OpenMIMS.desktop
+echo "TryExec=$(readlink -e ~/bin/Fiji.app/lib/runOpenMIMS.sh)" >> ~/.local/share/applications/OpenMIMS.desktop
+
+echo "application/x-extension-im=OpenMIMS.desktop" >> ~/.local/share/applications/mimeapps.list
+echo "application/x-extension-nrrd=OpenMIMS.desktop" >> ~/.local/share/applications/mimeapps.list
+
+echo "${green} Done.${reset}"
+
