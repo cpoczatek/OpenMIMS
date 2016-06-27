256c28f1f7002fe089225ab513d5d35175075f36
taylorwrt <taylorwrt@wrt-macbook-pro.partners.org>
2016-06-27 15:27:26 -0400

    Added document listener to the textArea, and implement the required methods (changedUpdate, removeUpdate, and insertUpdate (which sets the new variable isModified). Added isModified and setIsModified methods.    These allow UI.java to force the update of a mims or nrrd file if its associated notes change.

--- src/com/nrims/imageNotes.java
+++ src/com/nrims/imageNotes.java
@@ -1,18 +1,22 @@
 package com.nrims;
 
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
 /**
  * The imageNotes class provides the user with an interface
  * for writing and storing notes.
  *
  * @author cpoczatek
  */
-public class imageNotes extends com.nrims.PlugInJFrame {
+public class imageNotes extends com.nrims.PlugInJFrame implements DocumentListener  {
 
     java.awt.Frame instance;
     private javax.swing.JTextArea textArea;
     private javax.swing.JScrollPane scrollPane;
+    private boolean isModified = false;
 
-    /** Constructor. Instatiates class.*/
+    /** Constructor. Instantiates class.*/
     public imageNotes() {
         super("Image Notes:");
         super.setDefaultCloseOperation(PlugInJFrame.HIDE_ON_CLOSE);
@@ -24,10 +28,34 @@ public class imageNotes extends com.nrims.PlugInJFrame {
         textArea.setColumns(50);
         textArea.setLineWrap(true);
         textArea.setWrapStyleWord(true);
+        
+        textArea.getDocument().addDocumentListener(this);
 
         scrollPane.setViewportView(textArea);
 
     }
+    
+    public void changedUpdate(DocumentEvent evt) {
+        isModified = true;
+    }
+        
+    public void removeUpdate(DocumentEvent e) {
+        isModified = true;
+    }
+    
+    public void insertUpdate(DocumentEvent evt) {
+//        DocumentEvent.EventType type = evt.getType();
+//        String typeString = null;
+//        if (type.equals(DocumentEvent.EventType.CHANGE)) {
+//          typeString = "Change";
+//        }  else if (type.equals(DocumentEvent.EventType.INSERT)) {
+//          typeString = "Insert";
+//        }  else if (type.equals(DocumentEvent.EventType.REMOVE)) {
+//          typeString = "Remove";
+//        }
+        isModified = true;    // gets called when imageNotes in instantiated, also when user types into the textArea.
+    }
+
 
     /**
      * Gets the text entered into the text area.
@@ -48,5 +76,23 @@ public class imageNotes extends com.nrims.PlugInJFrame {
      */
     public void setOutputFormatedText(String text) {
         textArea.setText(text.replaceAll("&/&/&","\n"));
+    }
+    
+     /**
+     * Returns true if the text of the note has been changed.
+     *
+     * @return isModified
+     */
+    public boolean isModified() {
+        return isModified;
+    }
+    
+     /**
+     * Sets the isModified flag to true or false.
+     *
+     * @param modified
+     */
+    public void setIsModified(boolean modified) {
+        isModified = modified;
     }
 }
