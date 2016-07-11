93cfeaf5ae1ae63aae3956b267754858b78291ab
taylorwrt <taylorwrt@wrt-macbook-pro.partners.org>
2016-07-11 14:20:36 -0400

    Reformatted using the NetBeans formatter to fix bad indents and apply other source code formatting.

--- src/com/nrims/segmentation/ClassManager.java
+++ src/com/nrims/segmentation/ClassManager.java
@@ -8,88 +8,101 @@ import java.util.ArrayList;
  * @author sreckow
  */
 public class ClassManager implements Cloneable {
+
     private ArrayList<SegClass> classes;
-            
+
     public ClassManager() {
         classes = new ArrayList<SegClass>();
     }
-    
-    public boolean addClass(String className){
+
+    public boolean addClass(String className) {
         SegClass segClass = getSegClass(className);
-        if(segClass != null)return false;
-        else{
+        if (segClass != null) {
+            return false;
+        } else {
             classes.add(new SegClass(className));
             return true;
         }
     }
-    
-    public boolean removeClass(String className){
+
+    public boolean removeClass(String className) {
         SegClass segClass = getSegClass(className);
-        if(segClass == null)return false;
-        else{
+        if (segClass == null) {
+            return false;
+        } else {
             classes.remove(segClass);
             return true;
         }
     }
-    
-    public boolean addRoi(String className, Roi roi){
+
+    public boolean addRoi(String className, Roi roi) {
         SegClass segClass = getSegClass(className);
-        if(segClass == null)return false;
-        else{
+        if (segClass == null) {
+            return false;
+        } else {
             segClass.addRoi(roi);
             return true;
         }
     }
-    
-    public String[] getClasses(){
+
+    public String[] getClasses() {
         String[] names = new String[classes.size()];
-        for(int i=0; i< names.length; i++) names[i] = classes.get(i).name;
+        for (int i = 0; i < names.length; i++) {
+            names[i] = classes.get(i).name;
+        }
         return names;
     }
-    
-    public SegRoi[] getRois(String className){     
+
+    public SegRoi[] getRois(String className) {
         SegClass segClass = getSegClass(className);
-        if(segClass == null) return new SegRoi[0];        
-        else {
+        if (segClass == null) {
+            return new SegRoi[0];
+        } else {
             SegRoi[] rois = new SegRoi[0];//segClass.rois.size()];
             return segClass.rois.toArray(rois);
         }
     }
-   
-    public boolean renameClass(String oldName, String newName){
+
+    public boolean renameClass(String oldName, String newName) {
         SegClass segClass = getSegClass(oldName);
-        if(segClass == null || oldName.equals(newName) || getSegClass(newName) != null) return false;
-        else{
+        if (segClass == null || oldName.equals(newName) || getSegClass(newName) != null) {
+            return false;
+        } else {
             segClass.name = newName;
             return true;
         }
     }
-    
-    private SegClass getSegClass(String className){
-        for(SegClass segClass: classes) if (segClass.name.equals(className)) return segClass;
+
+    private SegClass getSegClass(String className) {
+        for (SegClass segClass : classes) {
+            if (segClass.name.equals(className)) {
+                return segClass;
+            }
+        }
         return null;
     }
-    
-    private class SegClass{
+
+    private class SegClass {
+
         private String name;
         private ArrayList<SegRoi> rois;
-        
-        public SegClass(String name){
+
+        public SegClass(String name) {
             this.name = name;
             rois = new ArrayList<SegRoi>();
         }
-        
-        public void addRoi(Roi roi){
-            rois.add(new SegRoi(roi,name,rois.size()+1));
-        }        
+
+        public void addRoi(Roi roi) {
+            rois.add(new SegRoi(roi, name, rois.size() + 1));
+        }
     }
 
     @Override
-    public Object clone(){
-       ClassManager newObject = new ClassManager();
-        for(String segClass : this.getClasses()){
+    public Object clone() {
+        ClassManager newObject = new ClassManager();
+        for (String segClass : this.getClasses()) {
             newObject.addClass(segClass);
-            for(SegRoi segRoi : this.getRois(segClass)){
+            for (SegRoi segRoi : this.getRois(segClass)) {
                 newObject.addRoi(segClass, segRoi.getRoi());
             }
         }
