72aee585a965fc2e56d0dae0ace28f797273c0cd
taylorwrt <taylorwrt@wrt-macbook-pro.partners.org>
2016-07-11 14:23:09 -0400

    Reformatted using the NetBeans formatter to fix bad indents and apply other source code formatting.

--- src/com/nrims/segmentation/SegRoi.java
+++ src/com/nrims/segmentation/SegRoi.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.nrims.segmentation;
 
 import ij.gui.Roi;
@@ -12,42 +11,45 @@ import ij.gui.ShapeRoi;
  *
  * @author sreckow
  */
-   public class SegRoi{               
-        private Roi roi;
-        private ShapeRoi shapeRoi;    
-        private String className;
-        private int ID;
-     
-        public SegRoi(Roi roi, String className, int ID) {            
-            this.className = className;
-            this.ID = ID;
-            setRoi(roi,null);
-        }                
+public class SegRoi {
 
-        public String getClassName() {
-            return className;
-        }
+    private Roi roi;
+    private ShapeRoi shapeRoi;
+    private String className;
+    private int ID;
 
-        public int getID(){
-            return ID;
-        }        
-
-        public Roi getRoi() {
-            return roi;
-        }
-
-        public void setRoi(Roi roi){
-            setRoi(roi, null);            
-        }
-
-        public void setRoi(Roi roi, ShapeRoi shape){
-            this.roi = roi;
-            this.shapeRoi = shape;
-            roi.setName(String.valueOf(ID));
-        }
-
-        public ShapeRoi getShapeRoi(){
-            if(shapeRoi==null) shapeRoi = new ShapeRoi(roi);
-            return shapeRoi;
-        }
+    public SegRoi(Roi roi, String className, int ID) {
+        this.className = className;
+        this.ID = ID;
+        setRoi(roi, null);
     }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public Roi getRoi() {
+        return roi;
+    }
+
+    public void setRoi(Roi roi) {
+        setRoi(roi, null);
+    }
+
+    public void setRoi(Roi roi, ShapeRoi shape) {
+        this.roi = roi;
+        this.shapeRoi = shape;
+        roi.setName(String.valueOf(ID));
+    }
+
+    public ShapeRoi getShapeRoi() {
+        if (shapeRoi == null) {
+            shapeRoi = new ShapeRoi(roi);
+        }
+        return shapeRoi;
+    }
+}
