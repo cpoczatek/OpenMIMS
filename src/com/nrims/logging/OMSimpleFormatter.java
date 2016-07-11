c284cc1e4db12b267bbabb0eda01d3b37fb9a85d
taylorwrt <taylorwrt@wrt-macbook-pro.partners.org>
2016-07-11 14:10:08 -0400

    Reformatted using the NetBeans formatter to fix bad indents and apply other source code formatting.

--- src/com/nrims/logging/OMSimpleFormatter.java
+++ src/com/nrims/logging/OMSimpleFormatter.java
@@ -3,6 +3,7 @@
  * and open the template in the editor.
  */
 package com.nrims.logging;
+
 import java.util.logging.*;
 
 /**
@@ -10,16 +11,16 @@ import java.util.logging.*;
  * @author cpoczatek
  */
 public class OMSimpleFormatter extends SimpleFormatter {
-    
+
     @Override
     public String format(LogRecord rec) {
         String message = "OpenMIMS: ";
-        message += "["+rec.getLevel().toString() + "] ";
+        message += "[" + rec.getLevel().toString() + "] ";
         message += rec.getMessage() + " : ";
         message += rec.getSourceClassName() + " ";
         message += rec.getSourceMethodName();
         message += "\n";
         return message;
     }
-    
+
 }
